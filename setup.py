deed503bc14193dd5bb652595a0a7e9d5892c857
Alex Ball <alex-ball@users.noreply.github.com>
2020-04-17 10:53:49 +0100

    Add minimal packaging information

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rdamsc',
+    version='2.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>= 3.6',
+    install_requires=[
+        'flask',
+        'Flask-WTF',
+        'flask-login',
+        'Flask-OpenID',
+        'rauth',
+        'oauth2client',
+        'flask-httpauth',
+        'passlib',
+        'tinydb',
+        'tinyrecord',
+        'rdflib',
+        'dulwich',
+        'flask-cors',
+    ],
+)
