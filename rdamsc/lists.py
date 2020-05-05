c0790b4cc5ff72b1712a65538e00921bfa49d18b
Alex Ball <alex-ball@users.noreply.github.com>
2020-05-05 17:06:16 +0100

    Add remaining record indexes, add group role support

--- rdamsc/lists.py
+++ rdamsc/lists.py
@@ -39,36 +39,53 @@ def get_scheme_tree(records: List[Scheme]) -> Mapping[str, str]:
 
 # List of standards
 # =================
+@bp.route('/<string:series>-index/<string:role>')
 @bp.route('/<string:series>-index')
-def record_index(series=None):
+def record_index(series=None, role=None):
     '''The contents template takes a 'tree' variable, which is a list of
     dictionaries, each with keys 'name' (human-readable name) and 'url'
     (Catalog page URL). The dictionary represents a node and if the node has
     children, the dictionary has a 'children' key, the value of which is
     another tree.
     '''
+    heading = series
     if series is None:
         abort(404)
     elif series == "scheme":
         # Listing metadata schemes.
+        rel = Relation()
+
         # Get all of them:
-        records = Scheme.all()
+        if role:
+            if not role.endswith("scheme"):
+                abort(404)
+            heading = role
+            records = rel.object_records(predicate=role)
+        else:
+            heading = "metadata standard"
+            records = Scheme.all()
 
         # Get blacklist of child schemes
-        rel = Relation()
         children = rel.subjects(predicate="parent scheme")
 
         # Assemble tree of records that are not on blacklist:
         tree = get_scheme_tree([
             record for record in records if record.mscid not in children])
         return render_template(
-            'contents.html', title='Index of metadata standards', tree=tree)
+            'contents.html', title=f"Index of {heading}s", tree=tree)
 
     # Listing another type of record.
     for record_cls in Record.__subclasses__():
         if series == record_cls.series:
             # Get all of them in alphabetical order:
-            records = record_cls.all()
+            if role:
+                if role not in ['maintainer', 'funder', 'user', 'originator']:
+                    abort(404)
+                heading = role
+                rel = Relation()
+                records = rel.object_records(predicate=role)
+            else:
+                records = record_cls.all()
             records.sort(key=lambda k: k.name)
             tree = [{
                 'name': record.name,
@@ -76,7 +93,8 @@ def record_index(series=None):
                     'main.display', table=record.table, number=record.doc_id)
                 } for record in records]
             return render_template(
-                'contents.html', title=f'Index of {series}s', tree=tree)
+                'contents.html', title=f"Index of {heading}s", tree=tree)
             break
     else:
         abort(404)
+
